5e1d26e6ce2a7f788e785d286a4c5e2046f3d2c6
mikesobczak <msobczak@yahoo.com>
2014-02-18 15:45:49 -0500

    Creates new class

---
+++ us.msobczak.git.first/src/us/msobczak/git/first/NewExampleClass.java
@@ -0,0 +1,13 @@
+package us.msobczak.git.first;
+
+public class NewExampleClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
